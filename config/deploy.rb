96f45aafcbbc66ed577ec5a3462a036ca8cb7e3a
aquosua <ackam.noble@gmail.com>
2023-09-25 13:32:18 +0000

    seeding

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,14 +18,9 @@ ask :branch, "main"
 
 # load 'lib/deploy/seed'
 namespace :deploy do
+  desc "reload the database with seed data"
   task :seed do
-    on primary fetch(:migration_role) do
-      within release_path do
-        with rails_env: fetch(:rails_env)  do
-          execute :rake, 'db:seed'
-        end
-      end
-    end
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
 end
 
