054a0a78349c078995140da9acb4c6987a458efd
aquosua <ackam.noble@gmail.com>
2023-10-09 15:05:29 +0000

    added default url to production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    include ActiveStorage::SetCurrent
     # before_action :authenticate_user!
     
     include Pagy::Backend
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
 
     def image_render(image, w = "100%", h = "100%", options = {})
         if image.representable?
-            image.representation(resize_to_fill: [w, h])
+            image.representation(resize_to_fill: [w, h]).processed.url
         else
              'sample.png'
         end 
