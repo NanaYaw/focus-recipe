2d46fd2e614d4a3ed86fffc9625d23d069eaac55
NanaYaw <ackam.noble@gmail.com>
2024-04-30 15:39:08 +0000

    Update admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,15 @@
 class Admin < ApplicationRecord
+  rolify
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  after_create :assign_default_role
+
+  private
+
+  def assign_default_role
+    add_role(:author) if roles.blank?
+  end
 end
