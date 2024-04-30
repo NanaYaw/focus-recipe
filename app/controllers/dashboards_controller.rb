b4eb39510368981360f666ea2a54cc4661d47a86
NanaYaw <ackam.noble@gmail.com>
2024-04-30 15:38:36 +0000

    Update dashboards_controller.rb

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,11 @@
 class DashboardsController < ApplicationController
+    include Pundit::Authorization
     layout 'application_admin'
     
+    def pundit_user
+        current_admin
+    end
+
     def index
     end
 end
