e2424dcba3cc07354d95cc36d5a1c7ca7244e0a0
NanaYaw <ackam.noble@gmail.com>
2024-04-30 15:38:58 +0000

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,35 @@
 module ApplicationHelper
-    include Pagy::Frontend
+  include Pagy::Frontend
 
-    def render_flash_stream
-        turbo_stream.update "flash", partial: "application/flash"
-    end
-    
-    def comment
-    end
+  def render_flash_stream
+    turbo_stream.update "flash", partial: "application/flash"
+  end
 
-    def image_render(image, w = "100%", h = "100%", options = {})
-        if image.representable?
-            image.variant(resize_to_fill: [w, h], format: :jpg, saver: { strip: true, quality: 50 }).processed.url
-        else
-            'sample.png'
-        end 
-    end
+  def comment
+  end
 
-    def profile_image
-        if current_user.profile.user_profile_image.attached?
-            image_tag current_user.profile.user_profile_image, size: "40", class: "mask is-squircle bg-gray-200"
-        else
-            image_tag 'user-1.png', size: "40", class: "mask is-squircle bg-gray-200 p-1"
-        end
+  def image_render(image, w = "100%", h = "100%", options = {})
+    if image.representable?
+      image.variant(resize_to_fill: [w, h], format: :jpg, saver: {strip: true, quality: 50}).processed.url
+    else
+      "sample.png"
     end
-    
-    def full_title(page_title = '')                    
-        base_title = "Ama Boadiwaa App." 
-        if page_title.blank?                             
-            base_title                                     
-        else
-            page_title + " | " + base_title
-        end                 
-    end
+  end
 
+  def profile_image
+    if current_user.profile.user_profile_image.attached?
+      image_tag current_user.profile.user_profile_image, size: "40", class: "mask is-squircle bg-gray-200"
+    else
+      image_tag "user-1.png", size: "40", class: "mask is-squircle bg-gray-200 p-1"
+    end
+  end
+
+  def full_title(page_title = "")
+    base_title = "Focus App."
+    if page_title.blank?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
-
-
