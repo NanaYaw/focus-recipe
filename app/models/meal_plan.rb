4075baa5e5169423ed13a9bf36f33af5dce4c0da
NanaYaw <ackam.noble@gmail.com>
2024-04-30 15:39:12 +0000

    Update meal_plan.rb

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,11 +1,11 @@
 class MealPlan < ApplicationRecord
   belongs_to :plan
   belongs_to :recipe, optional: true
- 
-  enum meal_type: Hash[MealType::MEAL_TYPE.collect{|type| [type, type]}], _prefix: true
+
+  enum meal_type: Hash[MealType::MEAL_TYPE.collect { |type| [type, type] }], _prefix: true
 
   scope :cooking, -> { sum(:number_of_persons_to_be_served) || 0 }
-  
+
   default_scope { order(day: :asc) }
 
   # after_update_commit { broadcast_replace_to "mealplans_list" }
