15c5a7d913bea0bccb1b7f971003ec2d3a5fc1a1
Nana Yaw <ackam.noble@gmail.com>
2026-06-06 16:21:39 +0000

    refactor: move users sub-module onto its own and remove admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,64 @@
 Rails.application.routes.draw do
   # Devise routes for users and admins
   devise_for :users, path: "users", controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations",
-    passwords: "users/passwords",
-    confirmations: "users/confirmations"
-  }
-
-  devise_for :admins, path: "admins", controllers: {
-    sessions: "admins/sessions",
-    passwords: "admins/passwords",
-    invitations: "admins/invitations"
+    sessions: "sessions",
+    registrations: "registrations",
+    passwords: "passwords",
+    confirmations: "confirmations"
   }
 
   
-  # Admin Routes
-  devise_scope :admin do
-    authenticated :admin do
-      # API Routes
-      namespace :api do 
-        namespace :v1 do
-          namespace :admins do
-            resources :recipes, only: [] do
-              collection do
-                get :new_title
-                post :create_title
-                get :edit_title
-              end
+  # # Admin Routes
+  # devise_scope :admin do
+  #   authenticated :admin do
+  #     # API Routes
+  #     namespace :api do 
+  #       namespace :v1 do
+  #         namespace :admins do
+  #           resources :recipes, only: [] do
+  #             collection do
+  #               get :new_title
+  #               post :create_title
+  #               get :edit_title
+  #             end
 
-              member do
-                patch :update_title
-                patch :create_ingredient
-                get :new_ingredient
-                patch :edit_ingredient
-                get :delete_ingredient
-                patch :create_directions
-                get :edit_direction
-                patch :update_direction
-                patch :delete_direction
-              end
-            end
-          end
-        end
-      end
+  #             member do
+  #               patch :update_title
+  #               patch :create_ingredient
+  #               get :new_ingredient
+  #               patch :edit_ingredient
+  #               get :delete_ingredient
+  #               patch :create_directions
+  #               get :edit_direction
+  #               patch :update_direction
+  #               patch :delete_direction
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
 
-      get "dashboard", to: "admins/dashboard#index", as: :authenticated_root
-      namespace :admins do
-        # Dashboard and admin management
-        resources :admins, except: [:create, :new]
+  #     get "dashboard", to: "admins/dashboard#index", as: :authenticated_root
+  #     namespace :admins do
+  #       # Dashboard and admin management
+  #       resources :admins, except: [:create, :new]
         
-        # Resources management
-        resources :ingredient_states
-        resources :measurement_units
-        resources :groceries
-        resources :grocery_categories
-        resources :ingredients
+  #       # Resources management
+  #       resources :ingredient_states
+  #       resources :measurement_units
+  #       resources :groceries
+  #       resources :grocery_categories
+  #       resources :ingredients
 
-        # Recipes management
-        resources :recipes
-      end
-    end
-  end
+  #       # Recipes management
+  #       resources :recipes
+  #     end
+  #   end
+  # end
 
   # User Routes
-  devise_scope :user do
-    authenticated :user do
+  # devise_scope :user do
+    # authenticated :user do
       # API Routes
       namespace :api do
         resources :lazyloads, only: [:index], controller: "lazy_loads"
@@ -80,52 +74,42 @@ Rails.application.routes.draw do
       end
 
       # User-specific routes
-      namespace :users do
-        get "plans", to: "plans#index", as: :authenticated_root
+      get "plans", to: "plans#index", as: :authenticated_root
+
+      resources :meal_plans
+
+      resources :plans do
+        collection do
+          get "meal-plans"
+          get "meal-plans-content"
+          get "lazy-update"
+          patch :meal_update
+        end
+        
+        get "recipes/:id/meal_plan/:meal_plan_id", to: "recipes#show", as: "recipe", on: :collection
       end
 
-      scope module: :users do
-        resources :meal_plans
+      resources :groceries, only: [:index, :show], controller: "grocery_list"
+      resources :recipes, only: [:show, :index] do
+        resources :favorites, only: [:create, :destroy]
+        resources :reviews, only: [:create, :update] do
+          member do
+            patch "new"
+            post "new"
+          end
 
-        resources :plans do
           collection do
-            get "meal-plans"
-            get "meal-plans-content"
-            get "lazy-update"
-            patch :meal_update
-          end
-          
-          get "recipes/:id/meal_plan/:meal_plan_id", to: "recipes#show", as: "recipe", on: :collection
-        end
-
-        resources :groceries, only: [:index, :show], controller: "grocery_list"
-        resources :recipes, only: [:show, :index, :single] do
-          resources :favorites, only: [:create, :destroy]
-          resources :reviews, only: [:create, :update] do
-            member do
-              patch "new"
-              post "new"
-            end
-
-            collection do
-              post "reply"
-              patch "reply"
-              patch "update_rating"
-            end
+            post "reply"
+            patch "reply"
+            patch "update_rating"
           end
         end
-
-        get "grocery-list", to: "grocery_list#show"
       end
-    end
 
-    # Additional user routes
-    scope module: :users do
+      get "grocery-list", to: "grocery_list#show"
       get "recipes/single/:id", to: "recipes#single", as: :single_recipes
       get "recipes/single_post/:id", to: "recipes#single_post", as: :single_post_recipes
       resources :profiles, only: [:update, :edit, :show]
-    end
-  end
 
   # Additional Routes
   get "test", to: "home#testmailer"
